0d597b4f7eda8ff6d54c959e96cdaa49c9428a62
Mirek Zvolský <zvolsky@seznam.cz>
2020-02-03 21:14:36 +0100

    more on anonymized dump

--- anonymized_dump.py
+++ anonymized_dump.py
@@ -2,5 +2,7 @@ from pg_dump_anonymized.anonymized_dump import Dump as ExampleDump
 
 
 class Dump(ExampleDump):
+    dbname = 'opentrafficweb'
+
     def dump(self, *args, **kwargs):
         return super().dump(*args, **kwargs)

2b3f9f0c70a08e1ad324bfbdde302dad96a6ce6a
Mirek Zvolský <zvolsky@seznam.cz>
2020-03-11 20:23:17 +0100

    fix preklep + comment

--- pokus/views.py
+++ pokus/views.py
@@ -1,7 +1,7 @@
 from requests import get
 
 from django.contrib.gis.geoip2 import GeoIP2
-from django.httlp import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -11,6 +11,8 @@ def where(request):
     if ip == '127.0.0.1':
         ip = get('https://api.ipify.org').text
     city = g.city(ip)
+    # 'continent_code': 'EU', 'continent_name': 'Europe', 'country_code': 'CZ', 'country_name': 'Czechia',
+    # 'dma_code': None, 'is_in_european_union': True, 'postal_code': '130 00', 'region': '10', 'time_zone': 'Europe/Prague'}
     return HttpResponse('%s, %s, lat %s long %s' % (ip, city['city'], city['latitude'], city['longitude']))
 
 def _get_client_ip(request):

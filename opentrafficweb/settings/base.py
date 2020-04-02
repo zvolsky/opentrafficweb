8447ebf77164896edd344a4a75fe2e026bb4e8c3
Mirek Zvolský <zvolsky@seznam.cz>
2020-04-02 14:42:40 +0200

    nazdar

--- opentrafficweb/settings/base.py
+++ opentrafficweb/settings/base.py
@@ -82,7 +82,9 @@ ROOT_URLCONF = 'opentrafficweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(PROJECT_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- opentrafficweb/urls.py
+++ opentrafficweb/urls.py
@@ -1,18 +1,3 @@
-"""opentrafficweb URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
@@ -27,6 +12,7 @@ urlpatterns.extend(
     i18n_patterns(
         path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
         path('users/', include(('users.urls', 'users'), namespace='users')),
+        path('', include(('main.urls', 'main'), namespace='main')),
     )
 )
 
@@ -42,5 +28,5 @@ if settings.DEBUG_TOOLBAR:
     import debug_toolbar
 
     urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
+        url(r'^__debug__/', include(debug_toolbar.urls)),
     ]

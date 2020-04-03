1e61c9c1ac1a664c757362fc47aef9b42df4ddcb
Mirek Zvolský <zvolsky@seznam.cz>
2020-04-03 20:03:09 +0200

    vice geo v javascriptu

--- _geo/static/_geo/js/geoutils.js
+++ _geo/static/_geo/js/geoutils.js
@@ -1,32 +1,174 @@
-PointStack = function(interval) {
+// PointStack - saved geopoints
+// GeoUtils
+
+/*************
+* PointStack *
+*************/
+/* examples:
+azimuth(0)   // average azimuth
+azimuth()    // current azimuth
+speed_kmh(0) // average speed
+speed_kmh()  // current speed
+sec2hms(time_sec(0))  // full time
+distance_formatted(0) // full distance
+*/
+
+PointStack = function() {
     this.stack = [];
-    this.interval = interval;
-    this.beat = typeof interval === "number";
+    this.gu = null;  // methods which need this will instantiate GeoUtils here
 };
 
 PointStack.prototype = {
+    minTimeForSpeed: 3,     // refuse count speed (ei. return 0) if time interval is shorter
+
     clear: function() {
         this.stack = [];
+    },
+
+    push: function(lat, lon) {
+        this.stack.push([lat, lon, Date.now()]);
     },
 
     points: function() {
         return this.stack.length;
     },
+
+    fix_pointPar: function(point) {
+        if (typeof point === "undefined") {
+            let points = this.points();
+            return this.goodStartPointForCalc(points)  // give point >33m distant
+        } else if (point < 0) {
+            return Math.max(0, this.points() + point);
+        } else {
+            return point;
+        }
+    },
+
+    // time in seconds from some point (backwards if negative, for whole stack if undefined)
+    time_sec: function(point) {
+        if (!this.points()) {
+            return 0;
+        }
+        point = this.fix_pointPar(point);
+        let n = Date.now();
+        return Math.round((n - this.stack[point][2]) / 1000);
+    },
+
+    // sec -> n:nn:nn
+    sec2hms: function(sec) {
+        let min = Math.floor(sec/60);
+        sec = sec % 60;
+        if (min >= 60) {
+            return '' + Math.floor(min/60) + ':' + ("0" + (min % 60)).slice(-2) + ':' + ("0" + sec).slice(-2);
+        } else {
+            return '' + min + ':' + ("0" + sec).slice(-2);
+        }
+    },
+
+    // TODO last time -> last points (or index of the starting point)
+
+    // speed from some point to end (backwards if negative, for whole stack if undefined)
+    speed_ms: function(point) {
+        let points = this.points();
+        if (points <= 1) {
+            return 0;
+        }
+        point = this.fix_pointPar(point);
+        let time = this.time_sec(point);
+        if (time < this.minTimeForSpeed) {
+            return 0;
+        }
+        let dist = this._distance(point, points);
+        return dist * 1000 / time;
+    },
+
+    speed_kmh: function(point) {
+        return this.speed_ms(point) * 3.6;
+    },
+
+    azimuth: function(point) {
+        let points = this.points();
+        if (points <= 1) {
+            return '*';
+        }
+        point = this.fix_pointPar(point);
+        let start = this.stack[point];
+        let stop = this.stack[points - 1];
+        return this.getGU().azimuth(start[0], start[1], stop[0], stop[1])
+    },
+
+    distance_formatted: function(point) {
+        let dist = this.distance(point);
+        if (dist < 1) {
+            return '' + Math.round(dist / 1000) + ' m';
+        } else {
+            return '' + Math.round(dist * 100) / 100 + ' km'
+        }
+    },
+
+    distance: function(point) {
+        let points = this.points();
+        if (points <= 1) {
+            return 0;
+        }
+        point = this.fix_pointPar(point);
+        return this._distance(point, points);
+    },
+
+    _distance: function(point, points) {
+        let start = this.stack[point];
+        let stop = this.stack[points - 1];
+        return this.getGU().distance(start[0], start[1], stop[0], stop[1]);
+    },
+
+    goodStartPointForCalc: function(points) {
+        let stop = this.stack[points - 1];
+        let absLat = Math.abs(stop[0]);
+        let lonCoef = (90 - absLat) / 60;  // multiply longitude Δ to get better estimate with regard to latitude
+        let minDist = 0.0003; // such Δ of lat/lon is about 33m and we use this to take well far starting point
+        for (let i = points-2; i >=0; i--) {
+            let thisOne = this.stack[i];
+            let estimate1 = Math.abs(thisOne[0] - stop[0] + 0.3 * lonCoef * (thisOne[1] - stop[1]));
+            let estimate2 = Math.abs(0.3 * (thisOne[0] - stop[0]) + lonCoef * (thisOne[1] - stop[1]));
+            if (Math.max(estimate1, estimate2) >= minDist) {
+                return i;
+            }
+        }
+        return 0;
+    },
+
+    // property behaviour for this.gu
+    getGU: function() {
+        if (!this.gu) {
+            this.gu = new GeoUtils();
+        }
+        return this.gu;
+    }
 };
 
+/***********
+* GeoUtils *
+***********/
 GeoUtils = function() {
 };
 
 GeoUtils.prototype = {
     // https://www.movable-type.co.uk/scripts/latlong.html
-    R = 6371; // in km; 6371e3 in metres
+    R: 6371, // in km; 6371e3 in metres
+
+    toRadians: function(arg) {
+        return arg * Math.PI / 180;
+    },
+    toDegrees: function(arg) {
+        return arg * 180 / Math.PI;
+    },
 
     // haversine formula to calculate the great-circle distance between two points
     distance: function(lat1, lon1, lat2, lon2) {
-        var φ1 = lat1.toRadians();
-        var φ2 = lat2.toRadians();
-        var Δφ = (lat2-lat1).toRadians();
-        var Δλ = (lon2-lon1).toRadians();
+        var φ1 = this.toRadians(lat1);
+        var φ2 = this.toRadians(lat2);
+        var Δφ = this.toRadians(lat2-lat1);
+        var Δλ = this.toRadians(lon2-lon1);
         var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
                 Math.cos(φ1) * Math.cos(φ2) *
                 Math.sin(Δλ/2) * Math.sin(Δλ/2);
@@ -37,10 +179,11 @@ GeoUtils.prototype = {
 
     // Spherical Law of Cosines
     // ... While simpler, the law of cosines is slightly slower than the haversine, in my tests
+    // see also: https://www.geodatasource.com/developers/javascript
     distance_slc: function(lat1, lon1, lat2, lon2) {
-        var φ1 = lat1.toRadians();
-        var φ2 = lat2.toRadians();
-        var Δλ = (lon2-lon1).toRadians();
+        var φ1 = this.toRadians(lat1);
+        var φ2 = this.toRadians(lat2);
+        var Δλ = this.toRadians(lon2-lon1);
         var d = Math.acos( Math.sin(φ1)*Math.sin(φ2) + Math.cos(φ1)*Math.cos(φ2) * Math.cos(Δλ) ) * this.R;
         return d;
     },
@@ -48,10 +191,10 @@ GeoUtils.prototype = {
     // Equirectangular approximation
     // ... fast ; Praha-Brno 184 km cca +10m
     distance_era: function(lat1, lon1, lat2, lon2) {
-        var φ1 = lat1.toRadians();
-        var φ2 = lat2.toRadians();
-        var λ1 = lon1.toRadians();
-        var λ2 = lon2.toRadians();
+        var φ1 = this.toRadians(lat1);
+        var φ2 = this.toRadians(lat2);
+        var λ1 = this.toRadians(lon1);
+        var λ2 = this.toRadians(lon2);
         var x = (λ2-λ1) * Math.cos((φ1+φ2)/2);
         var y = (φ2-φ1);
         var d = Math.sqrt(x*x + y*y) * this.R;
@@ -64,13 +207,13 @@ GeoUtils.prototype = {
         return (this.bearing(lat1, lon1, lat2, lon2) + 360) % 360;
     },
     bearing: function(lat1, lon1, lat2, lon2) {      // <-180, +180>, 0 to the North, + to the East
-        return this.bearing_rad(lat1, lon1, lat2, lon2).toDegrees();
+        return this.toDegrees(this.bearing_rad(lat1, lon1, lat2, lon2));
     },
     bearing_rad: function(lat1, lon1, lat2, lon2) {  // <-PI, +PI>, 0 to the North, + to the East
-        var φ1 = lat1.toRadians();
-        var φ2 = lat2.toRadians();
-        var λ1 = lon1.toRadians();
-        var λ2 = lon2.toRadians();
+        var φ1 = this.toRadians(lat1);
+        var φ2 = this.toRadians(lat2);
+        var λ1 = this.toRadians(lon1);
+        var λ2 = this.toRadians(lon2);
         var y = Math.sin(λ2-λ1) * Math.cos(φ2);
         var x = Math.cos(φ1)*Math.sin(φ2) - Math.sin(φ1)*Math.cos(φ2)*Math.cos(λ2-λ1);
         var brng = Math.atan2(y, x);
@@ -78,32 +221,6 @@ GeoUtils.prototype = {
     },
 
     // https://www.movable-type.co.uk/scripts/latlong.html (end)
-
-    /*
-    // Spherical Law of Cosines
-    // LGPLv3 : https://www.geodatasource.com/developers/javascript
-    distance: function(lat1, lon1, lat2, lon2, unit) {  // unit: (K)ilometers, (M)iles, (N)autilus
-        if ((lat1 == lat2) && (lon1 == lon2)) {
-            return 0;
-        }
-        else {
-            var radlat1 = Math.PI * lat1/180;
-            var radlat2 = Math.PI * lat2/180;
-            var theta = lon1-lon2;
-            var radtheta = Math.PI * theta/180;
-            var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-            if (dist > 1) {
-                dist = 1;
-            }
-            dist = Math.acos(dist);
-            dist = dist * 180/Math.PI;
-            dist = dist * 60 * 1.1515;
-            if (unit=="K") { dist = dist * 1.609344 }
-            if (unit=="N") { dist = dist * 0.8684 }
-            return dist;
-        }
-    }
-    */
 
     k2m: function(k) {return k / 1.609344},
     m2k: function(m) {return m * 1.609344},
--- main/static/main/js/home.js
+++ main/static/main/js/home.js
@@ -2,6 +2,7 @@ Page_Home = function() {
     this.timeBeat = 250;  // ms
     this.geoBeat = 15;    // s
     this.beatsToGeo = 1000 / this.timeBeat * this.geoBeat
+    this.ps = new PointStack();
 };
 
 Page_Home.prototype = {
@@ -16,36 +17,48 @@ Page_Home.prototype = {
         } else {
             return [pos, ''];
         }
-    }
+    },
     
     // https://github.com/perfectline/geopoint
     getCurrentPositionCallback: function(position) {
         let lat = this.latLonToNESW(position.coords.latitude, 'NS');
         let lon = this.latLonToNESW(position.coords.longitude, 'EW');
-        let point = new GeoPoint(lon[0], lat[0]);
+        let point = new GeoPoint(Math.abs(lon[0]), Math.abs(lat[0]));
         $('#lat').html(point.getLatDeg() + ' ' + lat[1]);
         $('#lon').html(point.getLonDeg() + ' ' + lon[1]);
-    }
+
+        this.ps.push(lat[0], lon[0]);
+        $('#azimuth').html(this.ps.azimuth(0));
+        $('#azimuth_now').html(this.ps.azimuth());
+        $('#speed').html(this.ps.speed_kmh(0));
+        $('#speed_now').html(this.ps.speed_kmh());
+        $('#duration').html(this.ps.sec2hms(this.ps.time_sec(0)));
+        $('#distance').html(this.ps.distance_formatted(0));
+    },
     
     getLocation: function() {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this.getCurrentPositionCallback);
+            navigator.geolocation.getCurrentPosition(this.getCurrentPositionCallback.bind(this));
         }
-    }
+    },
     
     updateTime: function() {
-        var counter = this.beatsToGeo;  // make the first immediatelly (xx+1 > xx)
-        setInterval(function () {
+        // TODO: better bind ?
+        var getLocation = this.getLocation.bind(this);
+        var beatsToGeo = this.beatsToGeo;  // make the first immediatelly (xx+1 > xx)
+        var counter = beatsToGeo;  // make the first immediatelly (xx+1 > xx)
+        var timer = setInterval(function () {
             d = new Date();
             $('#time').html(
                 d.toLocaleDateString().replace(/\s/g, "").replace(d.getFullYear().toString(), "") +
                 ' <b>' + d.toLocaleTimeString() + '</b>')
     
             counter += 1;
-            if (counter > this.beatsToGeo) {
-                this.getLocation();
+            if (counter > beatsToGeo) {
+                getLocation();
                 counter = 0;
             }
+            // clearInterval(timer) // debug getLocation()
         }, this.timeBeat);
     }
 };
